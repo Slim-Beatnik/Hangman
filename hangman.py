4031c066e0698876db508d409717b7cd29be181b
Slim-Beatnik <totem64@gmail.com>
2025-05-30 20:36:58 -0700

    Updated error message to prev_turn_message - now includes correct guess message

--- hangman.py
+++ hangman.py
@@ -10,7 +10,7 @@ word = words[randint(0, len(words)-1)].upper()
 guessed_letters = []
 win = False
 fail_count = 0
-oops = ''
+prev_turn_message = ''
 display_word = "_ " * len(word)
 available_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
@@ -50,8 +50,9 @@ while fail_count != 6 and not win:
     # Show the word progress 
     # The underscore or the letter to show the progress of how many has been guessed 
     print(display_hangman(fail_count))
-    print(oops) # print error message
-    opps = '' # reset error message
+    if not win:
+        print(prev_turn_message) # print error message
+    prev_turn_message = '' # reset error message
     print(" ".join(display_word), '\n')
     
     print(" ".join([letter for letter in available_letters if letter not in guessed_letters]))
@@ -60,12 +61,12 @@ while fail_count != 6 and not win:
 
     # Add it to our guessed letters 
     if guess in guessed_letters:
-        oops = f"You already guessed this letter: {guess}"
+        prev_turn_message = f"You already guessed this letter: {guess}"
     else:
         guessed_letters.append(guess)
         if guess in word:
             count = word.count(guess)
-            print(f"Correct! {guess} is in the word exactly {count} time{'s' if count > 1 else ''}")
+            prev_turn_message = f"Correct! {guess} is in the word exactly {count} time{'s' if count > 1 else ''}"
             display_word = " ".join(map(lambda ch: ch if ch in guessed_letters else "_", word))
             win = '_' not in display_word
             
